ed88057c935ec5b6b098a1bf1c8f6cb32942da35
rpf5573 <rpf5573@gmail.com>
2020-10-31 00:21:22 +0900

    모르것다

--- src/components/point/index.js
+++ src/components/point/index.js
@@ -17,6 +17,12 @@ export default class Point extends React.Component {
     };
   }
   componentWillMount() {
+    /**
+     * 이 코드를 왜쓴거야? onChange에 this.props를 왜 넣은걸까?
+     * componentWillMount는 실제로 DOM에 붙기 직전에 호출된다.
+     * 왜 호출했을까?
+     * render하기 전에 호출이 되는구마잉. 이거 호출되자 마자 render가 호출되겠지
+     */
     this.onChange(this.props);
   }
   componentWillReceiveProps(nextProps) {
@@ -26,15 +32,24 @@ export default class Point extends React.Component {
     const props = this.props;
     return cur !== props.cur || point !== props.point || max !== props.max || !props.cur;
   }
+  /**
+   * 
+   * 여기 이 cur는 무엇을 의미하는걸까? 
+   */
   onChange({ cur, point, max }) {
     clearInterval(Point.timeout);
-    if (cur) { // 在游戏进行中
+    if (cur) { // 게임이 진행중이라면?
+
+      /**
+        * 아니 근데, componentWillMount에서도 onChange를 호출했는데, 이렇게 되면
+        * setState를 호출하는게 좀 오바마 아닌가? 
+      */
       this.setState({
         label: point >= max ? ZDF : DF,
         number: point,
       });
-    } else { // 游戏未开始
-      const toggle = () => { // 最高分与上轮得分交替出现
+    } else { // 게임이 시작하기 전이라면
+      const toggle = () => { // 최고득점과 이전 득점이 번갈아 가면서 나온다
         this.setState({
           label: SLDF,
           number: point,
@@ -48,7 +63,7 @@ export default class Point extends React.Component {
         }, 3000);
       };
 
-      if (point !== 0) { // 如果为上轮没玩, 也不用提示了
+      if (point !== 0) { // 지난라운드때 뭔가 해서 포인트가 있었다면, 전광판에 번갈아 띄워준다
         toggle();
       } else {
         this.setState({
@@ -58,6 +73,7 @@ export default class Point extends React.Component {
       }
     }
   }
+  
   render() {
     return (
       <div>
